b797b2c25c443d721142d85e88ae3174b88d95cb
Stewart ilondanga <stilondanga@gmail.com>
2020-09-06 14:08:14 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
 
 var app = new Vue({
@@ -17,53 +17,56 @@ var app = new Vue({
     selectedClass: "c-4",
     picture: [],
     initialPicture: [],
-    savedPicture: [] },
+    savedPicture: []
+  },
 
 
-  created: function () {
+  created: function() {
     this.picture = this.getInitialPicture();
     this.initialPicture = this.cloneArray(this.picture);
   },
 
   methods: {
 
-    save: function () {
+    save: function() {
       this.savedPicture = this.cloneArray(this.picture);
       console.log(this.savedPicture);
     },
 
-    load: function () {
+    load: function() {
       this.picture = this.cloneArray(this.savedPicture);
     },
 
-    reset: function () {
+    reset: function() {
       this.picture = this.cloneArray(this.initialPicture);
     },
 
-    clear: function () {
+    clear: function() {
       this.picture = this.cloneArray(this.getBlankPicture());
     },
 
-    cloneArray: function (item) {
+    cloneArray: function(item) {
       return JSON.parse(JSON.stringify(item));
     },
 
-    setSelectedClass: function (className) {
+    setSelectedClass: function(className) {
       this.selectedClass = className;
     },
 
-    updatePicture: function (position) {
+    updatePicture: function(position) {
       let selectedClass =
-      this.picture[position] == this.selectedClass ?
-      "c-8" :
-      this.selectedClass;
+        this.picture[position] == this.selectedClass ?
+        "c-8" :
+        this.selectedClass;
       this.$set(this.picture, position, selectedClass);
     },
 
-    getBlankPicture: function () {
+    getBlankPicture: function() {
       return ["c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8"];
     },
 
-    getInitialPicture: function () {
+    getInitialPicture: function() {
       return ["c-8", "c-8", "c-8", "c-3", "c-4", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-2", "c-3", "c-4", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-2", "c-3", "c-4", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-3", "c-4", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-3", "c-4", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-3", "c-4", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-3", "c-4", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-2", "c-3", "c-4", "c-5", "c-8", "c-8", "c-8", "c-8", "c-8", "c-2", "c-3", "c-4", "c-5", "c-8", "c-8", "c-8"];
-    } } });
+    }
+  }
+});
