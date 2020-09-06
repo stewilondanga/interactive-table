423a6b5ea37eee25e4f0ef82c8815ea871a06050
Stewart ilondanga <stilondanga@gmail.com>
2020-09-06 14:07:34 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,64 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+var app = new Vue({
+
+  el: "#app",
+
+  data: {
+    rows: 9,
+    cols: 9,
+    selectedClass: "c-4",
+    picture: [],
+    initialPicture: [],
+    savedPicture: [] },
+
+
+  created: function () {
+    this.picture = this.getInitialPicture();
+    this.initialPicture = this.cloneArray(this.picture);
+  },
+
+  methods: {
+
+    save: function () {
+      this.savedPicture = this.cloneArray(this.picture);
+      console.log(this.savedPicture);
+    },
+
+    load: function () {
+      this.picture = this.cloneArray(this.savedPicture);
+    },
+
+    reset: function () {
+      this.picture = this.cloneArray(this.initialPicture);
+    },
+
+    clear: function () {
+      this.picture = this.cloneArray(this.getBlankPicture());
+    },
+
+    cloneArray: function (item) {
+      return JSON.parse(JSON.stringify(item));
+    },
+
+    setSelectedClass: function (className) {
+      this.selectedClass = className;
+    },
+
+    updatePicture: function (position) {
+      let selectedClass =
+      this.picture[position] == this.selectedClass ?
+      "c-8" :
+      this.selectedClass;
+      this.$set(this.picture, position, selectedClass);
+    },
+
+    getBlankPicture: function () {
+      return ["c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8"];
+    },
+
+    getInitialPicture: function () {
+      return ["c-8", "c-8", "c-8", "c-3", "c-4", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-2", "c-3", "c-4", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-2", "c-3", "c-4", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-3", "c-4", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-3", "c-4", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-3", "c-4", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-3", "c-4", "c-8", "c-8", "c-8", "c-8", "c-8", "c-8", "c-2", "c-3", "c-4", "c-5", "c-8", "c-8", "c-8", "c-8", "c-8", "c-2", "c-3", "c-4", "c-5", "c-8", "c-8", "c-8"];
+    } } });
